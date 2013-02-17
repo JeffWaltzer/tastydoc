9da733b4f20cebe518233dbce29b82a1f6bc807d
Jeff Waltzer <jeffwaltzer@gmail.com>
2013-02-17 16:09:21 -0500

    Added erb into HtmlView, which cleaned it up considerably.

--- app/html_view.rb
+++ app/html_view.rb
@@ -1,17 +1,24 @@
+require 'erb'
+
 class HtmlView
   def initialize(document)
     @document= document
   end
 
   def render
-    <<-END.gsub(/^ {8}/, '')
+    template= <<-END.gsub(/^ {8}/, '')
         <html>
             <head>
             </head>
             <body>
-              #{@document[:contact] && "<div class='contactinfo'>#{@document[:contact]}</div>"}
+              <% if @document[:contact] %>
+                  <div class='contactinfo'>
+                      <%= @document[:contact] %>
+                  </div>
+              <% end %>
             </body>
         </html>
     END
+    ERB.new(template).result(binding)
    end
 end
