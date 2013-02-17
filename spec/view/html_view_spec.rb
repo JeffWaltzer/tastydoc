bf36b17c5ea25485300eb053862feff6eed863b6
Jeff Waltzer <jeffwaltzer@gmail.com>
2013-02-17 15:21:54 -0500

    Cleaned up the html view specs

--- spec/view/html_view_spec.rb
+++ spec/view/html_view_spec.rb
@@ -1,16 +1,25 @@
 require_relative '../../app/html_view'
 require 'nokogiri'
 
-describe "HtmlView#render" do
-  it "should create an HTML document" do
+describe "HtmlView#render's result" do
+  it "is an HTML document" do
     html= HtmlView.new({}).render
     html.gsub!(/\s+/, '')
     html.should == '<html><head></head><body></body></html>'
   end
+end
 
-  it "should render contact info as a div with class 'contactinfo'" do
+describe "The contact info div" do
+  before do
     doc= Nokogiri::HTML(HtmlView.new({contact: "Joe Smith"}).render)
-    doc.xpath("//div[@class='contactinfo']").should have(1).element
-    doc.xpath("//div[@class='contactinfo']")[0].text.should == 'Joe Smith'
+    @contactinfo= doc.xpath("//div[@class='contactinfo']")
+  end
+  
+  it "exists" do
+    @contactinfo.should have(1).element
+  end
+
+  it "has the correct contact info content" do
+    @contactinfo[0].text.should == 'Joe Smith'
   end
 end
