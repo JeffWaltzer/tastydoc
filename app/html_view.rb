aa445bf24677ab78e6a1d56c9afa84fe08dbf71c
Jeff Waltzer <jeffwaltzer@gmail.com>
2013-02-17 15:06:58 -0500

    More html view stuff; started adding content

--- app/html_view.rb
+++ app/html_view.rb
@@ -1,2 +1,17 @@
 class HtmlView
+  def initialize(document)
+    @document= document
+  end
+
+  def render
+    <<-END.gsub(/^ {8}/, '')
+        <html>
+            <head>
+            </head>
+            <body>
+              #{@document[:contact] && "<div class='contactinfo'>#{@document[:contact]}</div>"}
+            </body>
+        </html>
+    END
+   end
 end
--- spec/view/html_view_spec.rb
+++ spec/view/html_view_spec.rb
@@ -1,7 +1,16 @@
 require_relative '../../app/html_view'
+require 'nokogiri'
 
-describe HtmlView do
-  it "should exist" do
-    HtmlView.new.should be
+describe "HtmlView#render" do
+  it "should create an HTML document" do
+    html= HtmlView.new({}).render
+    html.gsub!(/\s+/, '')
+    html.should == '<html><head></head><body></body></html>'
+  end
+
+  it "should render contact info as a div with class 'contactinfo'" do
+    doc= Nokogiri::HTML(HtmlView.new({contact: "Joe Smith"}).render)
+    doc.xpath("//div[@class='contactinfo']").should have(1).element
+    doc.xpath("//div[@class='contactinfo']")[0].text.should == 'Joe Smith'
   end
 end
