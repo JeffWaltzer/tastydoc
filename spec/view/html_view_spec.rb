54a985771a496af3a1f60409ce7fb0efbebdde86
Jeff Waltzer <jeffwaltzer@gmail.com>
2013-02-17 20:15:04 -0500

    Cleaned up specs

--- spec/view/html_view_spec.rb
+++ spec/view/html_view_spec.rb
@@ -1,6 +1,13 @@
 require_relative '../../app/html_view'
 require 'nokogiri'
 
+def xpath_for(parent, *children)
+  children.inject("//div[@class='#{parent}']") do |path_so_far, child|
+    path_so_far + "/div[@class='#{child}']"
+  end
+end
+
+
 describe "HtmlView#render's result" do
   it "is an HTML document" do
     html= HtmlView.new({}).render
@@ -12,22 +19,22 @@ end
 describe "An empty contact" do
   before do
     master_document= {contact: {}}
-    doc= Nokogiri::HTML(HtmlView.new(master_document).render)
-    @contactinfo= doc.xpath("//div[@class='contact']")
-    @name= @contactinfo.xpath("//div[@class='name']")
-    @email= @contactinfo.xpath("//div[@class='email']")
+    @doc= Nokogiri::HTML(HtmlView.new(master_document).render)
   end
   
   it "exists" do
-    @contactinfo.should have(1).element
+    contact= @doc.xpath(xpath_for('contact'))
+    contact.should have(1).element
   end
 
   it "has the correct contact name" do
-    @name.should have(0).elements
+    name= @doc.xpath(xpath_for('contact', 'name'))
+    name.should have(0).elements
   end
 
   it "has the correct contact email" do
-    @email.should have(0).elements
+    email= @doc.xpath(xpath_for('contact', 'email'))
+    email.should have(0).elements
   end
 end
 
@@ -39,62 +46,54 @@ describe "A populated contact" do
         email: 'joe@example.com'
       }
     }
-    doc= Nokogiri::HTML(HtmlView.new(master_document).render)
-    @contactinfo= doc.xpath("//div[@class='contact']")
-    @name= @contactinfo.xpath("//div[@class='name']")
-    @email= @contactinfo.xpath("//div[@class='email']")
+    @doc= Nokogiri::HTML(HtmlView.new(master_document).render)
   end
   
   it "exists" do
-    @contactinfo.should have(1).element
+    contact= @doc.xpath(xpath_for('contact'))
+    contact.should have(1).element
   end
 
   it "has the correct contact name" do
-    @name[0].text.strip.should == 'Joe Smith'
+    name= @doc.xpath(xpath_for('contact', 'name'))
+    name[0].text.strip.should == 'Joe Smith'
   end
 
   it "has the correct contact email" do
-    @email[0].text.strip.should == 'joe@example.com'
+    email= @doc.xpath(xpath_for('contact', 'email'))
+    email[0].text.strip.should == 'joe@example.com'
+  end
+end
+
+def should_have_document_section(parent_node, child_node, expected_text)
+  describe child_node do
+    before do
+      @header= @doc.xpath(xpath_for(parent_node, child_node))
+    end
+
+    it "exists" do
+      @header.should have(1).element
+    end
+
+    it "has the correct text" do
+      @header.text.strip.should == expected_text
+    end
   end
 end
 
 describe "A summary" do
   before do
     master_document= {summary: 'I have done lots of stuff.'}
-    doc= Nokogiri::HTML(HtmlView.new(master_document).render)
-    @summary= doc.xpath("//div[@class='summary']")
+    @doc= Nokogiri::HTML(HtmlView.new(master_document).render)
   end
   
   it "exists" do
-    @summary.should have(1).element
+    summary= @doc.xpath(xpath_for('summary'))
+    summary.should have(1).element
   end
 
-  describe "header" do
-    before do
-      @summary_header= @summary.xpath("//div[@class='header']")
-    end
+  should_have_document_section('summary', 'header', 'Summary')
+  should_have_document_section('summary', 'text', 'I have done lots of stuff.')
 
-    it "exists" do
-      @summary_header.should have(1).element
-    end
-
-    it "has the correct text" do
-      @summary_header.text.strip.should == 'Summary'
-    end
-  end
-
-  describe "text" do
-    before do
-      @summary_text= @summary.xpath("//div[@class='text']")
-    end
-
-    it "exists" do
-      @summary_text.should have(1).element
-    end
-
-    it "has the correct text" do
-      @summary_text.text.strip.should == 'I have done lots of stuff.'
-    end
-  end
 end
 
