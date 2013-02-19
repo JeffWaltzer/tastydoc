8ed8ed9bfc3b89ccaa543b546d1ad6c9b0bb209b
Jeff Waltzer <jeffwaltzer@gmail.com>
2013-02-18 20:11:17 -0500

    Added some real content to the resume data.

--- data/jeffs_resume.rb
+++ data/jeffs_resume.rb
@@ -1,8 +1,43 @@
-JEFFS_RESUME= {
+﻿JEFFS_RESUME= {
   contact: {
-    name: 'Jeff Waltzer',
-    email: 'jeffwaltzer@gmail.com'
+    name: 'Jeffrey L. Waltzer',
+    street: '3 Riverview Court #202',
+    city_state_zip: 'Laurel, MD 20707',
+    phone: '240-671-9368 (cell)',
+    email: 'jeffwaltzer@gmail.com',
   },
-  summary: "Jeff is very experienced. Don't ask."
-}
-    
+
+  summary: 'Senior Ruby on Rails Programmer with extensive experience doing ground-up development for several mission-critical projects.   Responsible for the concept, coding, testing, implementation, user training, and technical support of new systems. Experienced with agile development methodologies and Behvaior Driven Development. ',
+
+  skills: {
+    header: 'Programming Languages, Databases, Tools',
+    text: 'Ruby 1.9.3, Ruby on Rails 3.2, RSpec, Jruby, jQuery, CoffeeScript, Haml, JavaScript, XML, MySQL, PostgreSQL, PostGIS, Oracle, Linux (Ubuntu, Red Hat, CentOS), Soap4r,  Java, J2EE, Hibernate, Eclipse IDE, JUnit, Assembler, RubyMine'},
+
+  education: 'BS, Computer Science, University of Bridgeport',
+
+  development: {
+    header: 'Professional Development',
+    text: ['Organizer for the Ruby on Rails code fest in Columbia, MD.',
+             'Projects developed included a Ruby refactoring tool, Podcast feed reader and web based GPS tracker.']},
+
+experience: [
+             {job_title: 'Senior Ruby on Rails Consultant',
+               company: 'Sol Systems (contract), Washington DC',
+               dates: '10/2012 – 2/2013',
+               responsibilities: ['Developed new software and helped maintained existing Ruby on Rails website used in day to day operations.',
+                                  'Advised on programming design principles.',
+                                  'Used Behavior Driven Development with Rspec to drive functionality and to ensure proper implementation of new features.',
+                                  'Refactored code to improve the system architecture and to make it more object oriented.',
+                                  'Used MiniProfiler and Bullet profiling tools to find inefficient database usage.',
+                                  'Converted HTML/Javascript into HAML/Coffeescript for improved code clarity.',
+                                  'Used JQuery Datatables and Ajax calls to improve user experience.']},
+
+             {job_title: 'Senior Ruby on Rails Developer',
+               company: 'Unissant (NASA contract), Riverdale, MD',
+               dates: '5/2011 – 7/2012',
+               respnsibilites: ["Ruby on Rails development for NASA's http://reverb.echo.nasa.gov website on an agile Scrum team. System used Ruby 1.9.2 (JRuby) , Ruby on Rails 3.1, Elastic Search, Cucumber, Oracle 11g, Jquery, GeoRuby, Tomcat, Git,  RESTful web services.",
+                                "Implemented interaction with ESI's RESTful web service API at both Javascript and Rails levels.  Used Jquery ajax calls to display remote system call progress.  Added dynamic display of images in a Jquery pop-up.",
+                                "Enhanced Jquery DataTables that displayed search results to give users the ability to resize/minimize columns, rearrange columns.  Also created UI element that did autocomplete on place names as they were typed in by the user.  Used Jasmine library to unit test Javascript code.",
+                                "Used Cucumber testing tool to verify ECHO Reverb at a functional level.  Rspec and MiniTest for testing at the unit level.",
+                                "Tutored fellow team members on unit testing and BDD (Behavior Driven Development)",
+                                "Frequently refactored code and advised team on system architecture, object oriented design, clean code concepts, unit testing with Behavior Driven Development and enhanced readability."]}]}
