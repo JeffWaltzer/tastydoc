6a015f7af1e1ac4d1f94d65eb3c79446ccd63872
Jeff Waltzer <jeffwaltzer@gmail.com>
2013-05-08 20:41:42 -0400

    Added spec for invalid document type

---
+++ spec/unit/invalid_document_throws_exception_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../spec_helper'
+require_relative '../../app/text_view'
+
+describe "rendering an invalid document" do
+  it "has the correct contact email" do
+    master_document= {
+      contact: {
+        name: 42,
+        email: 'joe@example.com'
+      }
+    }
+    expect { TextView.new(master_document).render }.to raise_error("Unknown document type: Fixnum")
+  end
+end
