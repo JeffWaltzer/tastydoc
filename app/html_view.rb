725f49d92727e380aff924ccde208fc7e17ff683
Jeff Waltzer <jeffwaltzer@gmail.com>
2013-11-04 20:22:26 -0500

    Moving methods from HtmlView over into RenderingContext.

--- app/html_view.rb
+++ app/html_view.rb
@@ -19,56 +19,39 @@ class HtmlView
   def render_content(content_name, document, indent= 0)
     context= RenderingContext.new(content_name, document, indent)
     if document.is_a?(String)
-      render_text(context)
+      context.render_text(self)
     elsif document.is_a?(Hash)
       if document[:link]
-        render_link(context)
+        context.render_link(self)
       else
-        render_hash(document, content_name, indent)
+        render_hash(context)
       end
     elsif document.is_a?(Array)
-      render_array(context)
+      context.render_array(self)
     end
   end
 
-  def render_array(context)
-    context.document.map do |element|
-      render_content(context.content_name, element, context.indent)
-    end.join('')
-  end
-
-  def render_link(context)
-    div_wrap(context.content_name,context.indent) do
-      document_text = context.document[:text] || context.document[:link]
-      "#{indent_string(context.indent+1)}<a href='#{context.document[:link]}'>#{document_text}</a>\n"
+  def render_hash(context)
+    div_wrap(context.content_name, context.indent) do |indent|
+      context.document.map { |key, value| render_content(key, value, indent) }.join('')
     end
-  end
-
-  def render_hash(content, content_name, indent)
-    div_wrap(content_name, indent) do |indent|
-      content.map { |key, value| render_content(key, value, indent) }.join('')
-    end
-  end
-
-  def render_text(context)
-    "#{open_div(context.content_name, context.indent)}#{indent_string(context.indent+1)}#{context.document}\n#{close_div(context.indent)}"
   end
 
   def indent_string(indent)
     "  " * indent
   end
 
-  def open_div(style, indent)
+  def begin_paragraph(style, indent)
     "#{indent_string(indent)}#{style ? "<div class='#{style}'>\n" : "<div>\n"}"
   end
 
-  def close_div(indent)
+  def end_paragraph(indent)
     "#{indent_string(indent)}</div>\n"
   end
 
   def div_wrap(style, indent)
     if style
-      "#{open_div(style, indent)}#{yield(indent+1)}#{close_div(indent)}"
+      "#{begin_paragraph(style, indent)}#{yield(indent+1)}#{end_paragraph(indent)}"
     else
       yield(indent)
     end
--- app/rendering_context.rb
+++ app/rendering_context.rb
@@ -1,2 +1,18 @@
 class RenderingContext < Struct.new(:content_name, :document, :indent)
+  def render_text(view)
+    "#{view.begin_paragraph(content_name, indent)}#{view.indent_string(indent+1)}#{document}\n#{view.end_paragraph(indent)}"
+  end
+
+  def render_link(view)
+    view.div_wrap(content_name,indent) do
+      document_text = document[:text] || document[:link]
+      "#{view.indent_string(indent+1)}<a href='#{document[:link]}'>#{document_text}</a>\n"
+    end
+  end
+
+  def render_array(view)
+    document.map do |element|
+      view.render_content(content_name, element, indent)
+    end.join('')
+  end
 end
