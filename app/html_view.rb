4fb8cf13ce58a7f88b1ea6a5934f586d393f485f
Jeff Waltzer <jeffwaltzer@gmail.com>
2013-11-04 20:37:53 -0500

    Moved render document and render hash to RenderingContext

--- app/html_view.rb
+++ app/html_view.rb
@@ -18,24 +18,9 @@ class HtmlView
 
   def render_content(content_name, document, indent= 0)
     context= RenderingContext.new(content_name, document, indent)
-    if document.is_a?(String)
-      context.render_text(self)
-    elsif document.is_a?(Hash)
-      if document[:link]
-        context.render_link(self)
-      else
-        render_hash(context)
-      end
-    elsif document.is_a?(Array)
-      context.render_array(self)
-    end
+    context.render_document(self)
   end
 
-  def render_hash(context)
-    div_wrap(context.content_name, context.indent) do |indent|
-      context.document.map { |key, value| render_content(key, value, indent) }.join('')
-    end
-  end
 
   def indent_string(indent)
     "  " * indent
@@ -49,7 +34,7 @@ class HtmlView
     "#{indent_string(indent)}</div>\n"
   end
 
-  def div_wrap(style, indent)
+  def paragraph(style, indent)
     if style
       "#{begin_paragraph(style, indent)}#{yield(indent+1)}#{end_paragraph(indent)}"
     else
--- app/rendering_context.rb
+++ app/rendering_context.rb
@@ -1,10 +1,13 @@
 class RenderingContext < Struct.new(:content_name, :document, :indent)
   def render_text(view)
-    "#{view.begin_paragraph(content_name, indent)}#{view.indent_string(indent+1)}#{document}\n#{view.end_paragraph(indent)}"
+    view.paragraph(content_name, indent) do
+      "#{view.indent_string(indent+1)}#{document}\n"
+    end
+  #  "#{view.begin_paragraph(content_name, indent)}#{view.indent_string(indent+1)}#{document}\n#{view.end_paragraph(indent)}"
   end
 
   def render_link(view)
-    view.div_wrap(content_name,indent) do
+    view.paragraph(content_name,indent) do
       document_text = document[:text] || document[:link]
       "#{view.indent_string(indent+1)}<a href='#{document[:link]}'>#{document_text}</a>\n"
     end
@@ -15,4 +18,26 @@ class RenderingContext < Struct.new(:content_name, :document, :indent)
       view.render_content(content_name, element, indent)
     end.join('')
   end
+
+  def render_hash(view)
+    view.paragraph(content_name, indent) do |indent|
+      document.map { |key, value| view.render_content(key, value, indent) }.join('')
+    end
+  end
+
+
+  def render_document(view)
+    if document.is_a?(String)
+      render_text(view)
+    elsif document.is_a?(Hash)
+      if document[:link]
+        render_link(view)
+      else
+        render_hash(view)
+      end
+    elsif document.is_a?(Array)
+      render_array(view)
+    end
+  end
+
 end
