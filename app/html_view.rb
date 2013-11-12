2a61c9336414806e58bee0d7e1a4c161d0234f2b
Jeff Waltzer <jeffwaltzer@gmail.com>
2013-11-11 21:09:33 -0500

    Moved string-manipulation code out of RenderingContext and into HtmlView

--- app/html_view.rb
+++ app/html_view.rb
@@ -3,22 +3,25 @@ require_relative "rendering_context"
 class HtmlView
   def initialize(style_sheet)
     @style_sheet= style_sheet
+    @accumulator = ''
   end
 
   def render(document)
+    @accumulator = ''
     context= RenderingContext.new(self, :document, document, 0)
-    begin_document() +
-        context.render_document +
-        end_document()
+    begin_document
+    context.render_document
+    end_document
+    @accumulator
   end
 
   def end_document
-    "  </body>\n" +
+    @accumulator += "  </body>\n" +
         "</html>\n"
   end
 
   def begin_document
-    "<html>\n" +
+    @accumulator += "<html>\n" +
         "  <head>\n" +
         "    <link href='tastydoc.css' rel='stylesheet' type='text/css'>\n" +
         "  </head>\n" +
@@ -26,28 +29,32 @@ class HtmlView
   end
 
   def link(document_text, document_link)
-    "<a href='#{document_link}'>" +
+    @accumulator += "<a href='#{document_link}'>" +
         document_text +
         "</a>\n"
   end
 
   def indent(level)
-    "  " * level
+    @accumulator += "  " * level
   end
 
   def begin_paragraph(style, level)
-    "#{indent(level)}#{style ? "<div class='#{style}'>\n" : "<div>\n"}"
+    indent(level)
+    @accumulator += "#{style ? "<div class='#{style}'>" : "<div>"}\n"
   end
 
   def end_paragraph(level)
-    "#{indent(level)}</div>\n"
+    indent(level)
+    @accumulator += "</div>\n"
   end
 
   def paragraph(style, level)
-    if style
-      "#{begin_paragraph(style, level)}#{yield(level+1)}#{end_paragraph(level)}"
-    else
-      yield(level)
-    end
+      begin_paragraph(style, level)
+      yield(level+1)
+      end_paragraph(level)
+  end
+
+  def string(s)
+    @accumulator += "#{s}\n"
   end
 end
--- app/rendering_context.rb
+++ app/rendering_context.rb
@@ -1,7 +1,8 @@
 class RenderingContext < Struct.new(:view, :content_name, :document, :indent)
   def render_text
     view.paragraph(content_name, indent) do
-      "#{view.indent(indent+1)}#{document}\n"
+      view.indent(indent+1)
+      view.string(document)
     end
   end
 
@@ -9,23 +10,24 @@ class RenderingContext < Struct.new(:view, :content_name, :document, :indent)
     view.paragraph(content_name, indent) do
       url = document[:link]
       text = document[:text] || url
-      view.indent(indent+1) + view.link(text, url)
+      view.indent(indent+1)
+      view.link(text, url)
     end
   end
 
   def render_array
-    document.map do |element|
+    document.each do |element|
       sub_context= RenderingContext.new(view, content_name, element, indent)
       sub_context.render_document
-    end.join('')
+    end
   end
 
   def render_hash
     view.paragraph(content_name, indent) do |indent|
-      document.map do |key, value|
+      document.each do |key, value|
         sub_context= RenderingContext.new(view, key, value, indent)
         sub_context.render_document
-      end.join('')
+      end
     end
   end
 
