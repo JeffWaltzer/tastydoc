daf8612c25c377d997de88e4e91a46e4d2fc912b
Jeff Waltzer <jeffwaltzer@gmail.com>
2014-01-20 21:26:13 -0500

    Refactored StringWrapper#wrap

--- app/string_wrapper.rb
+++ app/string_wrapper.rb
@@ -5,17 +5,17 @@ class StringWrapper
     @indent_string= indent_string
   end
 
+  def word_fits(line, word)
+    line.length + word.length + 1 < MAX_LINE_LENGTH
+  end
+
   def wrap(s)
     result= ''
-    words= s.split(/\s/)
+    words= s.split(/\s/).reverse
     while !words.empty?
-      line= @indent_string
-      while !words.empty? && (line.strip.empty? || line.length + words[0].length + 1 < MAX_LINE_LENGTH)
-        if !line.strip.empty?
-          line += ' '
-        end
-        line += words[0]
-        words= words[1..-1]
+      line= ''
+      while !words.empty? && (line.empty? || word_fits(line, words[-1]))
+        line += (line.empty? ? @indent_string : ' ') + words.pop
       end
       result += line + "\n"
     end
