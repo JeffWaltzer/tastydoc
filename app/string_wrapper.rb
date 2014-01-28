e75fdd8f458e4d075dd550eaaf41a4249a0d9a50
Jeff Waltzer <jeffwaltzer@gmail.com>
2014-01-27 20:30:09 -0500

    Refactored StringWrapper#wrap

--- app/string_wrapper.rb
+++ app/string_wrapper.rb
@@ -5,20 +5,20 @@ class StringWrapper
     @indent_string= indent_string
   end
 
-  def word_to_big(line, word)
+  def word_too_big(line, word)
     !line.empty? && line.length + word.length + 1 >= MAX_LINE_LENGTH
   end
 
   def wrap(s)
     result= ''
-    words= s.split(/\s/).reverse
-    until words.empty?
-      line= ''
-      until words.empty? || word_to_big(line, words[-1])
-        line += (line.empty? ? @indent_string : ' ') + words.pop
+    line= ''
+    s.split(/\s/).each do |word|
+      if word_too_big(line, word)
+        result += line + "\n"
+        line= ''
       end
-      result += line + "\n"
+      line += (line.empty? ? @indent_string : ' ') + word
     end
-    result
+    result + line + "\n"
   end
 end
